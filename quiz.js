d6d069bcbe3887d8e526bab3c5b0eec0ca202cfb
Rachel Johnson <rjohns0783@gmail.com>
2021-02-28 21:41:10 -0500

    Added Countdown to SpaceX Launch

--- quiz.js
+++ quiz.js
@@ -107,4 +107,22 @@ document.getElementById('submit').addEventListener('click', function() {
 });
 
 
+// ----- Start Countdown Code -----
+
+const countDownDate = new Date("April 20, 2021 16:00:00").getTime();
+const interval = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+
 
